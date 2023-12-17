7ac487934e4262d6bd9f716ad57d463a885a7aa7
KrisdaPhontre <s66309010002@kktech.ac.th>
2023-12-17 23:48:44 +0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -16,45 +16,56 @@ function myFunctions(i) {
       menu.className = "menu";
     }
 }
-// 
-const countdownEls = document.querySelectorAll(".countdown")
-countdownEls.forEach(countdownEl => createCountdown(countdownEl))
 
-function createCountdown(countdownEl){
-  const target = new Date(new Date(countdownEl.dataset.targetDate).toLocaleString('en', ))
-  const parts = {
-    days: {text: ["days","day"], dots: 30},
-    hours: {text: ["hours","hour"], dots: 24},
-    minutes: {text: ["minutes","minute"], dots: 60},
-    seconds: {text: ["seconds","second"], dots: 60},
-  }
-
-  Object.entries(parts).forEach(([key, value])=>{
-    const partEl = document.createElement("div");
-    partEl.classList.add("part", key);
-    partEl.style.setProperty("--dots", value.dots);
-    value.element = partEl;
-
-    const remainingEl = document.createElement("div");
-    remainingEl.classList.add("remaining");
-    remainingEl.innerHTML = `
-      <span class="number"></span>
-      <span class="text"></span>
-    `
-    partEl.append(remainingEl);
-    for(let i = 0; i < value.dots; i++){
-      const dotContainerEl = document.createElement("div");
-      dotContainerEl.style.setProperty("--dot-idx", i);
-      dotContainerEl.classList.add("dot-container")
-      const dotEl = document.createElement("div");
-      dotEl.classList.add("dot")
-      dotContainerEl.append(dotEl);
-      partEl.append(dotContainerEl);
-    }
-    countdownEl.append(partEl);
-  })
-  getRemainingTime(target, parts)
+function activeshowPage(){
+  let MyVar = setTimeout(showPage, 3000);
 }
+
+function showPage() {
+  document.getElementById('preLoader').style.display = 'none';
+  document.getElementById('pageContent').style.display = 'block';
+}
+
+
+// 
+// const countdownEls = document.querySelectorAll(".countdown")
+// countdownEls.forEach(countdownEl => createCountdown(countdownEl))
+
+// function createCountdown(countdownEl){
+//   const target = new Date(new Date(countdownEl.dataset.targetDate).toLocaleString('en', ))
+//   const parts = {
+//     days: {text: ["days","day"], dots: 30},
+//     hours: {text: ["hours","hour"], dots: 24},
+//     minutes: {text: ["minutes","minute"], dots: 60},
+//     seconds: {text: ["seconds","second"], dots: 60},
+//   }
+
+//   Object.entries(parts).forEach(([key, value])=>{
+//     const partEl = document.createElement("div");
+//     partEl.classList.add("part", key);
+//     partEl.style.setProperty("--dots", value.dots);
+//     value.element = partEl;
+
+//     const remainingEl = document.createElement("div");
+//     remainingEl.classList.add("remaining");
+//     remainingEl.innerHTML = `
+//       <span class="number"></span>
+//       <span class="text"></span>
+//     `
+//     partEl.append(remainingEl);
+//     for(let i = 0; i < value.dots; i++){
+//       const dotContainerEl = document.createElement("div");
+//       dotContainerEl.style.setProperty("--dot-idx", i);
+//       dotContainerEl.classList.add("dot-container")
+//       const dotEl = document.createElement("div");
+//       dotEl.classList.add("dot")
+//       dotContainerEl.append(dotEl);
+//       partEl.append(dotContainerEl);
+//     }
+//     countdownEl.append(partEl);
+//   })
+//   getRemainingTime(target, parts)
+// }
 
 function getRemainingTime(target, parts, first=true){
   const now = new Date()
@@ -85,13 +96,12 @@ function getRemainingTime(target, parts, first=true){
   }
 }
 
-// function myFunctions1(m) {
-//   m.classList.toggle("change");
-//countdown
-//   let bar = document.getElementById("baricon");
-//   if (bar.className === 'tapbar') {
-//     bar.className += ' tapbar-active'
-//   }   else {
-//     bar.className = "tapbar";
-//   }
-// }
+function myFunctions1(m) {
+  m.classList.toggle("P2-active");
+  let bar = document.getElementById("baricon");
+  if (bar.className === 'tapbar') {
+    bar.className += ' tapbar-active'
+   }   else {
+    bar.className = "tapbar";
+   }
+}
